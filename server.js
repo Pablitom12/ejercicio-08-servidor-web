8ff8360f028f2b0fb25d09b38802794fcab3b0eb
nahuel ferro <nahuelferro.2013@gmail.com>
2025-04-23 12:41:56 -0300

    arreglo error 404 y finalizacion tp

--- server.js
+++ server.js
@@ -1,20 +1,3 @@
-// const http = require("http");
-
-// // Creamos el servidor
-
-// const server = http.createServer((request, response)=>{
-
-// })
-
-
-// // Port del servidor
-// const PORT = 3000;
-
-// server.listen(PORT, () => {
-//     console.log(`Servidor escuchando en el puerto http://localhost:${PORT}`)
-// })
-
-
 
 const http = require('http');
 const fs = require('fs');
